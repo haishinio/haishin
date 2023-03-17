f926fa8cba47a94caf33a0ea709a1551ba9f367a
Thomas Moore <tomo@uchuu.io>
2023-03-17 13:15:20 +0000

    Mock out the archive api if in faker mode

--- pages/api/transcribe/archive.ts
+++ pages/api/transcribe/archive.ts
@@ -2,6 +2,9 @@
 import { v4 as uuidv4 } from 'uuid'
 import splitTranscribeTranslate from '../../../utils/split-transcribe-translate'
 
+import { faker as fakerGB } from '@faker-js/faker/locale/en_GB'
+import { faker as fakerJP } from '@faker-js/faker/locale/ja'
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type { TextLog } from '../../../types/Textlog'
 
@@ -29,12 +32,21 @@ export default async function handler(
   // For every 10s, split the file and send to whisper and then deepl
   for (let i = 0; i < numOfParts; i++) {
     const startTime = partDuration * i
-    const data = await splitTranscribeTranslate(
-      streamFile,
-      startTime.toString(),
-      '10', 
-      textLogArray.at(-1)?.transcription ?? ''
-    )
+
+    let data = { transcription: '', translation: '' }
+    if (process.env.APP_ENV !== 'faker') {
+      data = await splitTranscribeTranslate(
+        streamFile,
+        startTime.toString(),
+        '10', 
+        textLogArray.at(-1)?.transcription ?? ''
+      )
+    } else {
+      data = {
+        transcription: fakerJP.lorem.words(10),
+        translation: fakerGB.lorem.words(10),
+      }
+    }
 
     textLogArray.push({
       id: uuidv4(),
