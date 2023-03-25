2ccf4be12186fec2794b0b62e548d807e0e7dfda
Thomas Moore <tomo@uchuu.io>
2023-03-25 13:47:24 +0000

    Adds outputTracing includes to try get ffmpeg loaded

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
+  sentry: {
+    hideSourceMaps: true,
+  },
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  sentry: {
-    hideSourceMaps: true,
+  outputFileTracingIncludes: {
+    '/api/stream': ['./utils/split-transcribe-translate.ts'],
+    '/api/stream/duration': ['./utils/split-transcribe-translate.ts'],
+    '/api/transcribe/archive': ['./utils/split-transcribe-translate.ts'],
+    '/api/transcribe/live': ['./utils/split-transcribe-translate.ts'],
   },
 };
 
