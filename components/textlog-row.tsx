34606b10bac6c9cd5f16142cc4871da1b6c9e263
Thomas Moore <tomo@uchuu.io>
2023-03-30 18:34:26 +0100

    Better mobile styling for textrows

--- components/textlog-row.tsx
+++ components/textlog-row.tsx
@@ -8,12 +8,12 @@ export default function TextLogRow({ textLog }: Props) {
   return (
     <>
       <td className="p-2 w-8">{textLog.time}</td>
-      <td className="hidden xl:table-cell p-2">
+      <td className="table-cell sm:hidden xl:table-cell p-2">
         <p>{textLog.transcription}</p>
         <p>{textLog.translation}</p>
       </td>
-      <td className="table-cell xl:hidden p-2">{textLog.transcription}</td>
-      <td className="table-cell w-1/2 xl:hidden p-2">{textLog.translation}</td>
+      <td className="hidden sm:table-cell xl:hidden p-2">{textLog.transcription}</td>
+      <td className="w-1/2 hidden sm:table-cell xl:hidden p-2">{textLog.translation}</td>
     </>
   )
 }
