cc40ff568b2fa29e8440692e914e60acd8a15991
Thomas Moore <tomo@uchuu.io>
2023-04-01 18:33:15 +0100

    Hopefully stop overflows when on the stacked table cell

--- components/textlog-row.tsx
+++ components/textlog-row.tsx
@@ -8,12 +8,12 @@ export default function TextLogRow({ textLog }: Props) {
   return (
     <>
       <td className="p-2 w-8">{textLog.time}</td>
-      <td className="table-cell sm:hidden xl:table-cell p-2">
+      <td className="p-2 w-full table-cell sm:hidden xl:table-cell">
         <p>{textLog.transcription}</p>
         <p>{textLog.translation}</p>
       </td>
-      <td className="w-1/2 hidden sm:table-cell xl:hidden p-2">{textLog.transcription}</td>
-      <td className="w-1/2 hidden sm:table-cell xl:hidden p-2">{textLog.translation}</td>
+      <td className="p-2 w-1/2 hidden sm:table-cell xl:hidden">{textLog.transcription}</td>
+      <td className="p-2 w-1/2 hidden sm:table-cell xl:hidden">{textLog.translation}</td>
     </>
   )
 }
