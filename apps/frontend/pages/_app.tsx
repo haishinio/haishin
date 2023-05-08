1782787291aea077e8e5ac1499b3c828dee12646
Thomas Moore <tomo@uchuu.io>
2023-05-08 21:48:06 +0100

    Adds vconsole to try inspect logs on iOS

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
+import Script from 'next/script';
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      {/* @ts-ignore */}
+      <Script src='https://unpkg.com/vconsole@latest/dist/vconsole.min.js' onLoad={() => { const vConsole = new window.VConsole() }} />
+    </>
+  )
 }
 
 export default MyApp
