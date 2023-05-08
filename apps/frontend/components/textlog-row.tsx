145a09c3eba1deb875939644e766c35d1cd62b3d
Thomas Moore <tomo@uchuu.io>
2023-05-08 22:33:46 +0100

    New look xs tablerow

--- apps/frontend/components/textlog-row.tsx
+++ apps/frontend/components/textlog-row.tsx
@@ -7,13 +7,20 @@ type Props = {
 export default function TextLogRow({ textLog }: Props) {
   return (
     <>
-      <td className="p-2 w-20">{textLog.time}</td>
-      <td className="p-2 w-fit table-cell sm:hidden xl:table-cell">
+      <td className="p-2 w-full table-cell sm:hidden">
+        <p className="text-sm font-bold">{textLog.time}</p>
+        <p className="break-all">{textLog.transcription}</p>
+        <p className="break-words">{textLog.translation}</p>
+      </td>
+
+      <td className="p-2 w-20 hidden sm:table-cell">{textLog.time}</td>
+      <td className="p-2 break-all w-1/2 hidden sm:table-cell xl:hidden">{textLog.transcription}</td>
+      <td className="p-2 break-words w-1/2 hidden sm:table-cell xl:hidden">{textLog.translation}</td>
+  
+      <td className="p-2 w-fit hidden xl:table-cell">
         <p className="break-all w-full">{textLog.transcription}</p>
         <p className="break-words w-full">{textLog.translation}</p>
       </td>
-      <td className="p-2 break-all w-1/2 hidden sm:table-cell xl:hidden">{textLog.transcription}</td>
-      <td className="p-2 break-words w-1/2 hidden sm:table-cell xl:hidden">{textLog.translation}</td>
     </>
   )
 }
