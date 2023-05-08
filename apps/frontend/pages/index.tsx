8487d151233061eee4db3aec5ef32d7bab6f01d1
Thomas Moore <tomo@uchuu.io>
2023-05-08 22:14:15 +0100

    Fix submit placement within button on iOS

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage<HomeProps> = (props) => {
             <span className="sr-only">URL</span>
             <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Stream url" type="url" name="streamUrl" onChange={(e) => { updateStreamUrl(e.target.value) }}/>
           </label>
-          <Link href={`/stream/${cleanStreamUrl}`} className='ml-4 px-8 py-1 text-white rounded bg-sky-600 hover:bg-sky-700'>Submit</Link>
+          <Link href={`/stream/${cleanStreamUrl}`} className='ml-4 px-8 py-2 text-white rounded bg-sky-600 hover:bg-sky-700'>Submit</Link>
         </form>
 
         <div className="text-center">
