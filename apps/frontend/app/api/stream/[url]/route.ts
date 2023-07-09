59363116cd49e3b3f4e424debc6957a570af9898
Thomas Moore <tomo@uchuu.io>
2023-07-09 04:11:52 +0100

    Have the stream api use ranges
    Doesn't change much yet but might open the way for us to serve the videos ourselves rather than embed streams

--- apps/frontend/app/api/stream/[url]/route.ts
+++ apps/frontend/app/api/stream/[url]/route.ts
@@ -8,19 +8,40 @@ const pathToData = (restOfFilePath: string): string => {
 
 export async function GET(request: Request, { params }: { params: { url: string } }) {
   const { url } = params;
-  console.log({url});
   const filePath = pathToData(`./data/${url}`); // Set file path
+  const videoSize = fs.statSync(filePath).size;
 
   // Check if the file exists
   if (fs.existsSync(filePath)) {
-    // Set headers to force file download
-    const headers = new Headers();
-    headers.set('Content-Type', 'application/octet-stream');
-    headers.set('Content-Disposition', `attachment; filename="${url}"`);
+    const range = request.headers.get('Range');
+    const CHUNK_SIZE = 10 ** 6;
 
-    // Read the file and send it as the response
+    if (range) {
+      console.log({ range, videoSize });
+
+      const [start, end] = range.replace(/bytes=/, '').split('-');
+      const startByte = parseInt(start);
+      const endByte = end ? parseInt(end) : Math.min(startByte + CHUNK_SIZE, videoSize - 1);
+      const chunkSize = (endByte - startByte) + 1;
+
+      console.log({ startByte, endByte, chunkSize });
+
+      const stream = fs.createReadStream(filePath, { start: startByte, end: endByte });
+
+      const headers = new Headers();
+      headers.set('Content-Range', `bytes ${startByte}-${endByte}/${videoSize}`);
+      headers.set('Accept-Ranges', 'bytes');
+      headers.set('Content-Length', chunkSize.toString());
+      headers.set('Content-Type', 'video/mp4');
+      headers.set('Cache-Control', 'no-cache');
+      headers.set('Connection', 'keep-alive');
+
+      return new NextResponse(stream, { headers, status: 206 });
+    }
+
+    console.log('No range');
     const stream = fs.createReadStream(filePath);
-    return new NextResponse(stream, { headers });
+    return new NextResponse(stream, { headers: { 'Content-Length': videoSize, 'Content-Type': 'video/mp4' }, status: 200 });
   } else {
     return new NextResponse(null, { status: 404 });
   }
