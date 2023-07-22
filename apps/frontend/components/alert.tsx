6dc55f835e8df9a3b223921555234f6249facc95
Thomas Moore <tomo@uchuu.io>
2023-07-22 17:59:30 +0100

    Just to reassure users that they don't need to worry about it causing me a massive payment issue but just be careful

--- apps/frontend/components/alert.tsx
+++ apps/frontend/components/alert.tsx
@@ -2,7 +2,7 @@ const Alert = () => (
   <div className="border-2 border-sky-200 bg-sky-100 rounded text-center p-4 my-6" role="alert">
     <p>If you have recieved this link from Thomas then great!</p>
     <p>Thank you for trying out the website and I hope it does everthing you want.</p>
-    <p className="font-medium"><span>Please note:</span> Your usage does charge me so please do not use it for long streams if possible at this current stage 🙇‍♂️</p>
+    <p className="font-medium"><span>Please note:</span> While I do have payment caps in place your usage does charge me so please do not use it for long streams if possible at this current stage, so we can all use it throughout the month 🙇‍♂️</p>
   </div>
 )
 
