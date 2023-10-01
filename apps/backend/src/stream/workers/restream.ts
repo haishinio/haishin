10e072349b95c1c54cb8bc497c71b92453641364
Thomas Moore <thomas.moore@hey.com>
2023-10-01 12:53:09 +0100

    A mostly complete restreamer
    Still need to work on the hls restream and testing the end of stream process

--- apps/backend/src/stream/workers/restream.ts
+++ apps/backend/src/stream/workers/restream.ts
@@ -1,1 +1,103 @@
-// Downloads + Pipes the livestream from streamlink to an rtmp server via ffmpeg
+import fs from 'node:fs'
+import path from 'node:path'
+
+import { setArchivedFileName } from '@haishin/utils'
+
+// prevents TS errors
+declare var self: Worker
+
+async function restream(streamInfo: any) {
+  // Create the stream folder
+  console.log('Creating stream folder...')
+  fs.mkdirSync(streamInfo.folder)
+  console.log('Created stream folder...')
+
+  // Use streamlink to download the stream
+  const streamLinkArgs = [
+    streamInfo.originalUrl,
+    '--default-stream',
+    'best',
+    '-R',
+    streamInfo.file,
+    '-f',
+    '--retry-open',
+    '5'
+  ]
+  const streamLinkProcess = Bun.spawn(['streamlink', ...streamLinkArgs], {
+    onExit(proc, exitCode, signalCode, error) {
+      if (error) console.log(error)
+
+      // Wait 20 seconds and then kill the ffmpeg process
+      setTimeout(() => {
+        console.log('Killing ffmpeg process...')
+        ffmpegProcess.kill()
+      }, 20000)
+
+      // After 40 seconds, move the stream file to the backup folder
+      setTimeout(() => {
+        console.log('Moving stream file to backup folder...')
+
+        const backupFolder = path.join(streamInfo.folder, '../../backups')
+        const backupFile = `${backupFolder}/${setArchivedFileName(
+          streamInfo.originalUrl
+        )}`
+
+        if (!fs.existsSync(backupFolder)) {
+          fs.mkdirSync(backupFolder)
+        }
+
+        console.log({ backupFile, backupFolder, streamFile: streamInfo.file })
+
+        fs.renameSync(streamInfo.file, backupFile)
+      }, 40000)
+
+      // After 60 seconds, delete the stream folder
+      setTimeout(() => {
+        console.log('Deleting stream folder...')
+        fs.rmdirSync(streamInfo.folder, { recursive: true })
+      }, 60000)
+    }
+  })
+
+  // Use ffmpeg to restream the stream
+  const ffmpegArgs = [
+    '-re',
+    '-i',
+    'pipe:0',
+    '-c:v',
+    'copy',
+    '-c:a',
+    'copy',
+    '-f',
+    'hls',
+    '-hls_time',
+    '2',
+    '-hls_list_size',
+    '3',
+    '-hls_flags',
+    'delete_segments',
+    '-hls_playlist_type',
+    'event',
+    streamInfo.streamFile
+  ]
+  const ffmpegProcess = Bun.spawn(['ffmpeg', ...ffmpegArgs], {
+    stdin: 'pipe'
+  })
+
+  // Pipe the streamlink stdout to the ffmpeg stdin
+  for await (const chunk of streamLinkProcess.stdout) {
+    ffmpegProcess.stdin.write(chunk)
+  }
+
+  // For testing stop streamlink after 10 seconds
+  setTimeout(() => {
+    console.log('Killing streamlink process...')
+    streamLinkProcess.kill()
+  }, 10000)
+}
+
+self.onmessage = (event: MessageEvent) => {
+  if (event.data.command === 'start') {
+    restream(event.data.streamInfo)
+  }
+}
