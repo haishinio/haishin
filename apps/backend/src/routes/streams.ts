ea4aa4c462e5cb49207dbbaffcabfd367c7c0d2f
Thomas Moore <thomas.moore@hey.com>
2023-10-02 10:02:43 +0100

    Get the mount path from railway and use that

--- apps/backend/src/routes/streams.ts
+++ apps/backend/src/routes/streams.ts
@@ -1,15 +1,21 @@
 import fs from "node:fs";
+import path from "node:path";
 
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
 
-if (!fs.existsSync("/data/streams")) {
-  fs.mkdirSync("/data/streams");
+export const streamsFolder = path.join(
+  process.env.RAILWAY_VOLUME_MOUNT_PATH as string,
+  "streams"
+);
+
+if (!fs.existsSync(streamsFolder)) {
+  fs.mkdirSync(streamsFolder);
 }
 
 const streams = new Elysia().use(
   staticPlugin({
-    assets: "/data/streams",
+    assets: streamsFolder,
     prefix: "/streams",
     ignorePatterns: [".gitkeep"],
   })
