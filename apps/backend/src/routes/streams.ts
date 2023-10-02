8b4f699c35a0b98dd16585fcdd2a03149aa4b7e4
Thomas Moore <thomas.moore@hey.com>
2023-10-02 09:41:28 +0100

    Confirm the correct path

--- apps/backend/src/routes/streams.ts
+++ apps/backend/src/routes/streams.ts
@@ -3,13 +3,13 @@ import fs from "node:fs";
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
 
-if (!fs.existsSync("data/streams")) {
-  fs.mkdirSync("data/streams");
+if (!fs.existsSync("/data/streams")) {
+  fs.mkdirSync("/data/streams");
 }
 
 const streams = new Elysia().use(
   staticPlugin({
-    assets: "data/streams",
+    assets: "/data/streams",
     prefix: "/streams",
     ignorePatterns: [".gitkeep"],
   })
