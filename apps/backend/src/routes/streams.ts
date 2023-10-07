e1a2bd8ac98e83d7e56293264028ca4de3666ca4
Thomas Moore <thomas.moore@hey.com>
2023-10-07 10:50:35 +0100

    Fix issues with streams route
    Still won't work for the moment due to move from node-media-server

--- apps/backend/src/routes/streams.ts
+++ apps/backend/src/routes/streams.ts
@@ -1,24 +1,50 @@
-import fs from "node:fs";
-import path from "node:path";
+import fs from 'node:fs'
+import path from 'node:path'
 
-import { Elysia } from "elysia";
-import { staticPlugin } from "@elysiajs/static";
+import { Elysia } from 'elysia'
+import { staticPlugin } from '@elysiajs/static'
+
+import { getDuration, getPathsByUrl, urlUtils } from '@haishin/utils'
 
 export const streamsFolder = path.join(
   process.env.RAILWAY_VOLUME_MOUNT_PATH as string,
-  "streams"
-);
+  'streams'
+)
 
 if (!fs.existsSync(streamsFolder)) {
-  fs.mkdirSync(streamsFolder);
+  fs.mkdirSync(streamsFolder)
 }
 
-const streams = new Elysia().use(
-  staticPlugin({
-    assets: streamsFolder,
-    prefix: "/streams",
-    ignorePatterns: [".gitkeep"],
-  })
-);
+const streams = new Elysia()
+  .get('/streams', () => {
+    const streams = fs.readdirSync(streamsFolder)
 
-export default streams;
+    const enhancedStreamData = streams.map((stream) => {
+      const streamUrl = urlUtils.decodeUrl(stream)
+      const paths = getPathsByUrl(streamUrl)
+      const title = `${paths.site} - ${paths.user}`
+
+      return {
+        id: stream,
+        started: new Date(),
+        duration: 0,
+        title,
+        thumbnail: `${streamsFolder}/${stream}/stream.jpg`,
+        url: streamUrl,
+        viewers: 0
+      }
+    })
+
+    console.log({ streams, enhancedStreamData })
+
+    return enhancedStreamData
+  })
+  .use(
+    staticPlugin({
+      assets: streamsFolder,
+      prefix: '/streams',
+      ignorePatterns: ['.gitkeep']
+    })
+  )
+
+export default streams
