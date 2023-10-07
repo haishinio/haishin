3dcade5192f39d6bc6657a802c0b4191899cd591
Thomas Moore <thomas.moore@hey.com>
2023-10-07 13:20:43 +0100

    Transcriptions and Translations done

--- apps/backend/src/transcriber/index.ts
+++ apps/backend/src/transcriber/index.ts
@@ -3,10 +3,19 @@
 // Sends this transcription and translation to the client via websocket
 
 import fs from 'node:fs'
+
+import OpenAI from 'openai'
+import * as deepl from 'deepl-node'
+
 import splitter from './splitter'
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+const translator = new deepl.Translator(process.env.DEEPL_API_KEY as string)
+
 export const transcribeStream = async (
-  ws: any, // Would be nice to fix these types
+  server: any, // Would be nice to fix these types
   redisClient: any, // Would be nice to fix these types
   url: string,
   file: string,
@@ -22,7 +31,7 @@ export const transcribeStream = async (
   if (currentUsers <= 0) {
     console.log('No one is watching the stream, try again in 5 seconds')
     setTimeout(() => {
-      void transcribeStream(ws, redisClient, url, file, startTime, prompt)
+      void transcribeStream(server, redisClient, url, file, startTime, prompt)
     }, 5000)
   }
 
@@ -30,23 +39,63 @@ export const transcribeStream = async (
   console.log('Splitting the stream into an audio part')
   const { partFileName, nextStartTime } = await splitter(file, startTime)
 
-  console.log({ partFileName, nextStartTime })
+  if (partFileName !== '') {
+    console.log({ partFileName, nextStartTime })
 
-  // Send this part to opeanAi for transcription
+    // Send this part to opeanAi for transcription
+    let transcriptionText = ''
+    try {
+      const transcription = await openai.audio.transcriptions.create({
+        model: 'whisper-1',
+        file: fs.createReadStream(partFileName),
+        prompt,
+        language: 'ja',
+        response_format: 'json',
+        temperature: 0
+      })
+      transcriptionText = transcription.text
+    } catch (error: unknown) {
+      console.error({ error })
+    }
 
-  // As we have the transcription, we don't need the part any more so delete it
-  // if (partFileName) {
-  //   console.log("Deleting the part file");
-  //   fs.unlinkSync(partFileName);
-  // }
+    // As we have the transcription, we don't need the part any more so delete it
+    if (partFileName) {
+      console.log('Deleting the part file')
+      fs.unlinkSync(partFileName)
+    }
 
-  // Send this transcription to deepL for translation
+    // Send this transcription to deepL for translation
+    let translation = { text: '' }
+    if (transcriptionText !== '') {
+      try {
+        translation = await translator.translateText(
+          transcriptionText,
+          'ja',
+          'en-GB'
+        )
+      } catch (error: unknown) {
+        console.error({ error })
+      }
+    }
 
-  // Send the transcription and translation to the client
+    console.log({ transcriptionText, translation: translation.text })
+
+    // Send the transcription and translation to the client
+    const message = JSON.stringify({
+      type: 'transcription-translation',
+      content: {
+        id: crypto.randomUUID(),
+        startTime: startTime,
+        transcription: transcriptionText,
+        translation: translation.text
+      }
+    })
+    server.publish(url, message)
+  }
 
   // Repeat
   console.log('And repeat in a few seconds...')
   setTimeout(() => {
-    void transcribeStream(ws, redisClient, url, file, nextStartTime, prompt)
+    void transcribeStream(server, redisClient, url, file, nextStartTime, prompt)
   }, 5000)
 }
