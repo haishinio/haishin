9aa9df8c0ed5e883d38c2b3fa20484233342562e
Thomas Moore <thomas.moore@hey.com>
2023-10-09 19:25:24 +0100

    Fix util by removing unused pathToData from index

---
+++ .changeset/brown-beans-cheer.md
@@ -0,0 +1,5 @@
+---
+'@haishin/utils': patch
+---
+
+Fix util by removing unused pathToData from index
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,7 +4,5 @@ export {
   setArchivedFileName
 } from './set-file-name'
 export { getDuration } from './get-duration'
-export { pathToData } from './path-to-data'
 export { msToSeconds, secondsToDuration } from './seconds-to-duration'
-// export { encodeUrl, decodeUrl } from './url-utils'
 export * as urlUtils from './url-utils'
